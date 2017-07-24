0e7ea1e37635bcc0e1979c4730c16ca887e67f14
user <alphonsosensleyii@this-macbook.local>
2017-07-24 14:34:43 -0700

    added image view to project

--- Chelsea & Alphonso/ViewController.swift
+++ Chelsea & Alphonso/ViewController.swift
@@ -10,10 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dateLabelOutlet: UILabel!
+    let formatter = DateFormatter()
+    
+    @IBOutlet weak var mainImage: UIImageView!
+    
+    let userCalender = Calendar.current
+    
+    let requestedComponent : Set<Calendar.Component> = [
+        Calendar.Component.month,
+        Calendar.Component.day,
+        Calendar.Component.hour,
+        Calendar.Component.minute,
+        Calendar.Component.second
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mainImage.image = #imageLiteral(resourceName: "ChelseaAlphonso")
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(printTime), userInfo: nil, repeats: true)
+        
+        timer.fire()
+        
+            }
+   
+    func printTime() {
+        formatter.dateFormat = "MM/dd/yy hh:mm:ss a"
+        let startTime = Date()
+        let endTime = formatter.date(from: "04/21/18 12:00:00 a")
+        let timeDifference = userCalender.dateComponents(requestedComponent, from: startTime, to: endTime!)
+        
+        dateLabelOutlet.text = "\(timeDifference.month!) Months \(timeDifference.day!) Days \(timeDifference.minute!) Minutes \(timeDifference.second!) Seconds"
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
