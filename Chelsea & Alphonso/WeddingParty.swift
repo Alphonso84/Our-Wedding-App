51316d548c316415cfe381b4ac08c66936edf645
user <alphonsosensleyii@this-macbook.local>
2017-09-27 02:05:10 -0700

    Adding TableView

---
+++ Chelsea & Alphonso/WeddingParty.swift
@@ -0,0 +1,38 @@
+//
+//  WeddingParty.swift
+//  Chelsea & Alphonso
+//
+//  Created by user on 9/22/17.
+//  Copyright © 2017 Alphonso. All rights reserved.
+//
+
+import UIKit
+
+class WeddingParty {
+    
+    struct Friend {
+    var Name: String
+    var Age: Int
+    var face: UIImage
+    var Job: String
+            
+    init(Name: String, Age: Int, face: UIImage, Job: String) {
+    self.Name = Name
+    self.Age = Age
+    self.face = face
+    self.Job = Job
+            }
+}
+        //Friend Instances
+        var len: Friend = Friend(Name: "Len", Age: 32, face: #imageLiteral(resourceName: "Len"), Job: "Pharmacy Technician")
+        var neville: Friend = Friend(Name: "Neville", Age: 34, face: #imageLiteral(resourceName: "Neville"), Job: "Civil Engineer")
+        var jonathan: Friend = Friend(Name: "Jonathan", Age: 32, face: #imageLiteral(resourceName: "Jonathan"), Job: "Human Resources")
+        var joseph: Friend = Friend(Name: "Joseph", Age: 29, face: #imageLiteral(resourceName: "Joseph"), Job: "Best-Man")
+        var aaron: Friend = Friend(Name: "Aaron", Age: 33, face: #imageLiteral(resourceName: "Aaron"), Job: "Director")
+        
+        // ARRAY OF FRIENDS
+        lazy var groomsMen: [Friend] = [len, neville, jonathan, joseph, aaron]
+        lazy var bridesMaids: [Friend] = []
+}
+    
+
