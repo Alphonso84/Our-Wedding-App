e1c70c41bab3f3debe3732710232a9103bcbc817
user <alphonsosensleyii@this-macbook.local>
2017-11-11 22:29:00 -0800

    Update

--- Wedding.swift
+++ Wedding.swift
@@ -12,10 +12,12 @@ import UIKit
 struct friends {
     var name: String
     var job: String
+    var image: UIImage
     
-    init(name: String, job: String) {
+    init(name: String, job: String, image: UIImage) {
         self.name = name
         self.job = job
+        self.image = image
         
     }
 }
@@ -23,26 +25,26 @@ struct friends {
 
 class Wedding: UIViewController, UITableViewDataSource, UITableViewDelegate{
    
-    var sections = ["GroomsMen", "BridesMaids"]
+    let sections = ["GroomsMen", "BridesMaids"]
     var groomsMen: [friends] = []
     var bridesMaids: [friends] = []
     
-    let Aaron = friends(name: "Aaron", job: "Sales Director at Adidas")
-    let Neville = friends(name: "Neville", job: "Civil Engineer")
-    let Jonathan = friends(name: "Jonathan", job: "Pastor")
-    let Joseph = friends(name: "Joseph", job: "Best Man")
-    let Len = friends(name: "Len", job: "Pharmacy Technician")
+    let Aaron = friends(name: "Aaron Seabron", job: "Sales Director at Adidas", image: #imageLiteral(resourceName: "Aaron"))
+    let Neville = friends(name: "Neville Sumiran", job: "Civil Engineer", image: #imageLiteral(resourceName: "Neville.JPG"))
+    let Jonathan = friends(name: "Jonathan Overall", job: "Pastor", image: #imageLiteral(resourceName: "Jonathan.JPG"))
+    let Joseph = friends(name: "Joseph Sensley", job: "Best Man", image: #imageLiteral(resourceName: "Joseph.JPG"))
+    let Len = friends(name: "Len Lacanlale", job: "Pharmacy Technician", image: #imageLiteral(resourceName: "Len.JPG"))
     
    
-    let Gena = friends(name: "Gena", job: "")
-    let Chereese = friends(name: "Chereese", job: "")
-    let Alecia = friends(name: "Alecia", job: "")
-    let Kristy = friends(name: "Kristy", job: "")
-    let Blake = friends(name: "Blake", job: "")
-    let Ashley = friends(name: "Ashley", job: "")
-    let Danielle = friends(name: "Danielle", job: "")
-    let Brittney_C = friends(name: "Brittney Coleman", job: "")
-    let Brittney_R = friends(name: "Brittney Richardson", job: "")
+    let Gena = friends(name: "Gena Brown", job: "", image: #imageLiteral(resourceName: "Gena.JPG"))
+    let Chereese = friends(name: "Chereese Bedingfield Rowe", job: "", image:#imageLiteral(resourceName: "Chereese.JPG"))
+    let Alecia = friends(name: "Alecia Harris", job: "", image: #imageLiteral(resourceName: "Alecia.JPG"))
+    let Kristy = friends(name: "Kristy Singletary", job: "", image: #imageLiteral(resourceName: "Kristy.JPG"))
+    let Blake = friends(name: "Blake", job: "", image: #imageLiteral(resourceName: "Blake.JPG"))
+    let Ashley = friends(name: "Ashley Seabron", job: "", image: #imageLiteral(resourceName: "Ashley.JPG"))
+    let Danielle = friends(name: "Danielle Sensley", job: "Marketing Manager", image: #imageLiteral(resourceName: "Danielle.JPG"))
+    let Brittney_C = friends(name: "Brittney Coleman", job: "", image: #imageLiteral(resourceName: "BrittneyC.JPG"))
+    let Brittney_R = friends(name: "Brittney Richardson", job: "", image: #imageLiteral(resourceName: "BrittneyR.JPG"))
     
 
     
@@ -66,21 +68,24 @@ class Wedding: UIViewController, UITableViewDataSource, UITableViewDelegate{
     
    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
-    let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)   //dequeueReusableCell(withIdentifier: "myCell")
+    let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+    let sectionNumber = indexPath.section
     
-    if sections.count == groomsMen.count {
+    
+    if sectionNumber == 0 {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         cell.textLabel?.text = groomsMen[indexPath.row].name
         cell.detailTextLabel?.text = groomsMen[indexPath.row].job
+        cell.imageView?.image = groomsMen[indexPath.row].image
         return cell
     } else if
-        sections.count == bridesMaids.count {
+        sectionNumber == 1 {
         let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
         cell.textLabel?.text = bridesMaids[indexPath.row].name
-        
         cell.detailTextLabel?.text = bridesMaids[indexPath.row].job
+        cell.imageView?.image = bridesMaids[indexPath.row].image
         return cell
-    
+
     }
     
     return cell
@@ -97,6 +102,7 @@ class Wedding: UIViewController, UITableViewDataSource, UITableViewDelegate{
             }
             
         }
+
         
         func numberOfSections(in tableView: UITableView) -> Int {
             return sections.count
